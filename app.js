3a8cc5cc2011cb2d25fcd3f129f912cb0d05f42d
jjorda <jjordamontejano@gmail.com>
2025-01-08 00:29:13 +0100

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-const port = 3005
+const port = process.env.PORT || 3005;
 process.title = "hello";
 
 app.get('/', (req, res) => {
